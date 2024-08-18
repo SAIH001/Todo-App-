b022876148be2d0a7382efe4f9b37e5c7c1954fa
Syed Ali Imran Hussain <syedaliimranofficial@gmail.com>
2024-08-18 11:54:12 -0700

    Connect DB

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
-const app = express();
+const ConnectDB  = require("./config/DB");
 
+const app = express();
 app.use(express.json());
-// app.use(express.urlencoded({extended:false}));
+
+
+
 
 app.use('/api/todoapp',require('./routes/todoRoutes'))
 app.use('/api/useraccount',require('./routes/userAccountRoutes'))
@@ -14,5 +17,6 @@ app.use('/api/useraccount',require('./routes/userAccountRoutes'))
 
 
 app.listen(process.env.PORT || 5000,()=>{
+    ConnectDB();
     console.log(`Server is running on the port of ${process.env.PORT || 5000}`);
 })
